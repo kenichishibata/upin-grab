034205241c13c104cfaf355d524fb6fab2021dfb
kenichishibata31 <kenichi.shibata@fastretailing.com>
2015-08-13 10:31:18 +0900

    added monolog for logging

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 //Real escape string without mysql 
 function mysql_escape_mimic($inp) { 
     if(is_array($inp)) 
@@ -12,30 +13,35 @@ function mysql_escape_mimic($inp) {
 //URL getter starts here...
 $SITE_ROOT = "http://www.uniqlo.com/jp/sp";
  	if(isset($_GET['title'])){
-		$urltemp= "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-		$url = htmlspecialchars_decode($urltemp);
-		$title = htmlspecialchars_decode($_GET['title']);
-		$description =  htmlspecialchars_decode($_GET['description']);
-		$image = htmlspecialchars_decode($_GET['image']); 
-		$site_name = "Uniqlo";
-		$app_id = 176318562473204;
+		$urltemp		= "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+		$url 			= html_entity_decode($urltemp);
+		$title 			= htmlspecialchars_decode($_GET['title']);
+		$description 	= htmlspecialchars_decode($_GET['description']);
+		$image 			= htmlspecialchars_decode($_GET['image']); 
+		$app_id 		= 176318562473204;
 }
+
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
 	<title>Uniqlo Digital Chirashi</title>
-
+	<meta charset="<?php //echo mb_detect_encoding($title, "JIS, shift_JIS, UTF-8") ?>">
 	<!-- dynamic meta tags -->
 	<meta property="og:url" content="<?php echo mysql_real_escape_string($url);?>" />
 	<meta property="og:type" content="website" /> 
 	<meta property="og:title" content="<?php echo mysql_real_escape_string($title);?>" />
 	<meta property="og:description" content="<?php echo mysql_real_escape_string($description);?>" />
-	<meta property="og:image" content="<?php echo mysql_real_escape_string($image);?>" />
-	<meta property="og:site_name" content="<?php echo mysql_escape_mimic($site_name);?>">
+	<meta property="og:image" content="<?php echo mysql_escape_mimic($image);?>" />
+	<meta property="og:site_name" content="Uniqlo">
 	<!-- twitter specific meta tags -->
-	<meta name="twitter:card" content="product">
-	<meta name="twitter:site" content="@UNIQLO_JAPAN">
+
+	<!-- space for seo --><!-- 
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+	<meta name="copyright" content="" />
+	<meta name="application-name" content="" /> -->
 	</head>
 <body>
 </body>
