9eabce184fcf92a959a1e2b41e82cfb75d139e10
kenichishibata31 <kenichi.shibata@fastretailing.com>
2015-08-20 07:50:16 +0900

    minimized lines

--- index.php
+++ index.php
@@ -5,9 +5,9 @@ $SITE_ROOT = "http://www.uniqlo.com/jp/sp";
  if(isset($_GET['title'])){
 	$urltemp		="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
 	$url 			= htmlentities($urltemp, ENT_QUOTES);
-	$title 			= mysql_real_escape_string($_GET['title']);
-	$description 	= mysql_real_escape_string($_GET['description']);
-	$image 			= mysql_real_escape_string($_GET['image']); 
+	$title 			= $_GET['title'];
+	$description 	= $_GET['description'];
+	$image 			= $_GET['image']; 
 	$site_name 		= "Uniqlo";
 	$app_id 		= 176318562473204;
 }
