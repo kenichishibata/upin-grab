faca8f8bb21018c0af6dbc5fcbba46e4adaf8192
Kenichi Shibata <kenichi.shibata@fastretailing.com>
2015-09-03 09:09:10 +0900

    added SNS name support and Autoredirect if not done by akamai

--- index.php
+++ index.php
@@ -2,9 +2,10 @@
 	$urltemp		="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
 	$url 			= htmlentities($urltemp, ENT_QUOTES);
 
-header('Location : $url');
-$SITE_ROOT = "http://www.uniqlo.com/jp/sp";
-$array_replaceable = array("%20","+"," ","。","、","！","%E3%80%82","。","%E3%80%81","、","%EF%BC%81","！");
+	header('Location : $url');
+	header('Status: 200');
+	$SITE_ROOT = "http://www.uniqlo.com/jp/sp";
+	$array_replaceable = array("%20","+"," ","。","、","！","%E3%80%82","。","%E3%80%81","、","%EF%BC%81","！");
 
 
  if(isset($_GET['title'])){
