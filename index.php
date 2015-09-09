92e9e7eb2bc456232cc98d73d73a9bad208279de
Kenichi Shibata <kenichi.shibata@fastretailing.com>
2015-09-09 15:24:29 +0900

    cleaned code debugged image url error

--- index.php
+++ index.php
@@ -1,22 +1,24 @@
 <?php
-	$urltemp		="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-	$url 			= htmlentities($urltemp, ENT_QUOTES);
+header('Content-Type: text/html');
 
-	header('Location : $url');
-	header('Status: 200');
-	$SITE_ROOT = "http://www.uniqlo.com/jp/sp";
-	$array_replaceable = array("%20","+"," ","。","、","！","%E3%80%82","。","%E3%80%81","、","%EF%BC%81","！");
+$SITE_ROOT = "http://www.uniqlo.com/jp/sp";
+$array_ro = array("%20","+"," ","?^?^?","?^?^?","?^?","%E3%80%82","?^?^?","%E3%80%81","?^?^?","%EF%BC%81","?^?");
 
 
  if(isset($_GET['title'])){
-	// $urltemp		="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-	// $url 		= htmlentities($urltemp, ENT_QUOTES);
-	$title 			= $_GET['title'];
-	$description 	= $_GET['description'];
-	$image 			= $_GET['image']; 
-	$name  			= $_GET['name'];
-	$site_name 		= "Uniqlo";
-	$app_id 		= 176318562473204;
+        $urltemp                ="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+        $url                    = htmlentities($urltemp, ENT_QUOTES);
+        $title                  = $_GET['title'];
+        $description            = $_GET['description'];
+        $image                  = $_GET['image'];
+        $site_name              = "Uniqlo";
+        $app_id                 = 176318562473204;
+        include_once("contents.html");
+        echo 'success!';
+        echo $title;
+        echo $description;
+        echo $image;
 }
-		include_once("contents.html");
+else
+    	echo 'Something went wrong. mail kenichi.shibata@fastretailing.com';
 ?>
